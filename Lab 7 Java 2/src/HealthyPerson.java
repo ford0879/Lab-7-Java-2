2a9e07000183481eed71935eb53f193be2a01478
Derek Ford <derekford@ou.edu>
2019-03-07 16:10:55 -0600

    Fixed issues and now implements Person class

--- Lab 7 Java 2/src/HealthyPerson.java
+++ Lab 7 Java 2/src/HealthyPerson.java
@@ -1,10 +1,10 @@
 
-public class HealthyPerson 
+public class HealthyPerson extends Person
 {
 
 	public HealthyPerson(String name, int age, String reason)
 	{
-		
+		super(name, age);
 	}
 	
 	protected int compareToImpl(Person p)
--- Lab 7 Java 2/src/SickPerson.java
+++ Lab 7 Java 2/src/SickPerson.java
@@ -1,10 +1,10 @@
 
-public class SickPerson
+public class SickPerson extends Person
 {
 
-	public SickPerson(String name, int age, String reason)
+	public SickPerson(String name, int age, int severity)
 	{
-		
+		super(name, age);
 	}
 	
 	protected int compareToImpl(Person p)
